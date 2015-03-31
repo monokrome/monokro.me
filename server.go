ee425bdb784eb810744ad8345bf402d91e336db8
Brandon R. Stoner <monokrome@limpidtech.com>
2015-03-30 21:31:30 -0700

    Added simple server.

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := html.EscapeString(r.URL.Path)
+
+	if name == "" {
+		name = "World"
+	}
+
+	fmt.Fprintf(w, "Hello, %q", name)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

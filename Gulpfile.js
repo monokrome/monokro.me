8cd611f88b2b5043dc7cd1133998b6bc6d96a857
Brandon R. Stoner <monokrome@limpidtech.com>
2015-11-06 19:20:31 -0800

    Some crap

---
+++ Gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp'),
+      path = require('path'),
+      tsconfig = require('./tsconfig.json'),
+      plugins = require('gulp-load-plugins')();
+
+
+PUBLIC_PATH = 'dist';
+
+
+function makeArray(items) {
+  var index, result = [];
+  for (index in items) result.push(items[index]);
+  return result;
+}
+
+
+function publicPath() {
+  var suffix = path.join.apply(this, arguments);
+  return path.join.call(this, PUBLIC_PATH, suffix);
+}
+
+
+gulp.task('markup', function () {
+  gulp.src('src/**.html')
+    .pipe(gulp.dest(publicPath()))
+});
+
+
+gulp.task('stylesheets', function () {
+  gulp.src('src/**.css')
+    .pipe(gulp.dest(publicPath()))
+});
+
+
+gulp.task('scripts', function () {
+  gulp.src('src/components/*.ts', {
+    base: path.join(__dirname, 'dist'),
+  })
+    .pipe(plugins.typescript(tsconfig.compilerOptions))
+    .pipe(plugins.concat('index.js'))
+    .pipe(gulp.dest(publicPath()))
+});
+
+
+gulp.task('default', [
+  'scripts',
+  'stylesheets',
+  'markup',
+]);
---
+++ src/components/body.ts
@@ -0,0 +1,11 @@
+import {Component, bootstrap} from "angular2/angular2";
+
+
+@Component({
+    selector: "body",
+    template: "<h1>This is a body tag.</h1>",
+})
+class Body {}
+
+
+bootstrap(Body);

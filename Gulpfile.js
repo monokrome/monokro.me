aa1e3ed292b3d1b459afe9ca7f02ab134c4ec17f
Brandon R. Stoner <monokrome@limpidtech.com>
2015-11-06 19:59:16 -0800

    browsersync

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp'),
       path = require('path'),
       tsconfig = require('./tsconfig.json'),
-      plugins = require('gulp-load-plugins')();
+      plugins = require('gulp-load-plugins')(),
+      browserSync = require('browser-sync').create();
 
 
 PUBLIC_PATH = 'dist';
@@ -28,22 +29,43 @@ gulp.task('markup', function () {
 
 gulp.task('stylesheets', function () {
   gulp.src('src/**.css')
-    .pipe(gulp.dest(publicPath()))
+    .pipe(gulp.dest(publicPath()));
 });
 
+
+gulp.task('systemjs:crap', function () {
+  gulp.src('node_modules/systemjs/dist/system-polyfills.js')
+    .pip(gulp.dest(publicPath()));
+});
 
 gulp.task('scripts', function () {
-  gulp.src('src/components/*.ts', {
-    base: path.join(__dirname, 'src'),
-  })
+  var tsFilter = plugins.filter('**.ts', {
+    restore: true,
+  });
+
+  gulp.src([
+    'node_modules/systemjs/dist/system.js',
+    'src/components/*.ts',
+  ], {base: path.join(__dirname, 'src')})
     .pipe(plugins.typescript(tsconfig.compilerOptions))
     .pipe(plugins.concat('index.js'))
-    .pipe(gulp.dest(publicPath()))
+    .pipe(gulp.dest(publicPath()));
 });
 
 
-gulp.task('default', [
+gulp.task('once', [
   'scripts',
   'stylesheets',
   'markup',
-]);
+])
+
+
+gulp.task('server', ['once'], function () {
+  browserSync.init({
+    server: publicPath(),
+    open: false,
+  })
+});
+
+
+gulp.task('default', ['once']);

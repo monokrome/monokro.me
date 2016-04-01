8ca8c3fd402cfb6a497abcfbfb99eb1f740d00ad
Brandon R. Stoner <monokrome@limpidtech.com>
2016-04-01 15:56:41 -0700

    Initial not-so-great ng2 project.

---
+++ app/body.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'body',
+    template: '<h1>Hey, girl.</h1>'
+})
+export class BodyComponent {}
---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {BodyComponent} from './body.component';
+
+
+bootstrap(BodyComponent);

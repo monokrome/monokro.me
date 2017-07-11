b56b322bfd49cced84e7b4a10cd56047468891a9
Bailey Stoner <bailey@patreon.com>
2017-07-11 13:53:07 -0700

    Add initial setup.py file with aiohttp.

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+
+requires = [
+  'aiohttp'
+]
+
+
+setup(
+    name='mk',
+    version='0.11.0',
+    description='Exposing my website, one hacker at a time.',
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author='Bailey Stoner',
+    author_email='monokrome@limpidtech.com',
+    url='https://monokro.me/',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=requires,
+    test_suite="mk",
+    entry_points="""\
+    [paste.app_factory]
+    main = mk:main
+    """,
+)
+

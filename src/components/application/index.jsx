71f264b716a6634ba4198cd6cb75854b564f4df5
Bailey Stoner <monokrome@monokro.me>
2017-09-22 16:48:41 -0700

    Fix blurriness issues.

--- src/components/application/index.jsx
+++ src/components/application/index.jsx
@@ -1,90 +1,11 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withState } from "recompose";
-import styled from "styled-components";
+import React from "react";
 
-@withState("context", "setContext", null)
-class Application extends Component {
-  static propTypes = {
-    // Draw function
-    draw: PropTypes.func.isRequired,
-    gridSize: PropTypes.number.isRequired,
+import Blocks from "components/blocks";
 
-    // Classname from styled-components
-    className: PropTypes.string.isRequired,
+const draw = (x, y) => {
+  const time = +new Date();
+  if (Math.sin(time * x / 200) > 0.5) return { fillStyle: "steelblue" };
+  return { fillStyle: "#FF9900" };
+};
 
-    // Canvas element state
-    context: PropTypes.object,
-    setContext: PropTypes.func.isRequired
-  };
-
-  static defaultProps = {
-    gridSize: 15,
-
-    draw: (x, y) => {
-      // Example implementation for when one is not provided
-      const timestamp = +new Date();
-      if (((timestamp / 350) % x) % y === 0) return { fillStyle: "white" };
-      if (x % 2 && y % 2) return { fillStyle: "white" };
-      else if (x % 2 && y % 4) return { fillStyle: "orange" };
-      else if (y % 2 && x % 4) return { fillStyle: "purple" };
-      else if (y % 2) return { fillStyle: "rebeccapurple" };
-      else if (x % 4 && y % 2) return { fillStyle: "yellow" };
-      else return { fillStyle: "steelblue" };
-    }
-  };
-
-  draw = canvas => {
-    const { context, draw, gridSize } = this.props;
-
-    for (let x = 0; x < gridSize; ++x) {
-      for (let y = 0; y < gridSize; ++y) {
-        const xDelta = context.canvas.width / gridSize;
-        const yDelta = context.canvas.height / gridSize;
-
-        const updates = draw(x, y, context.canvas.width, context.canvas.height);
-        if (updates) Object.assign(context, updates);
-
-        context.fillRect(xDelta * x, yDelta * y, xDelta, yDelta);
-      }
-    }
-
-    this.animationRequestId = null;
-    this.requestAnimationFrame();
-  };
-
-  requestAnimationFrame = () => {
-    this.cancelAnimationFrame();
-    this.animationRequestId = requestAnimationFrame(this.draw);
-  };
-
-  cancelAnimationFrame = () => {
-    if (!this.animationRequestId) return;
-    cancelAnimationFrame(this.animationRequestId);
-    this.animationRequestId = null;
-  };
-
-  setContext = canvas => {
-    const { context, setContext } = this.props;
-    const newContext = canvas.getContext("2d");
-
-    if (context === newContext) return;
-
-    setContext(newContext);
-    this.requestAnimationFrame(this.draw);
-  };
-
-  componentWillUnmount() {
-    this.cancelAnimationFrame();
-  }
-
-  render() {
-    const { className } = this.props;
-    return <canvas className={className} ref={this.setContext} />;
-  }
-}
-
-export default styled(Application)`
-  height: 100vh;
-  width: 100vw;
-`;
+export default props => <Blocks draw={draw} />;
---
+++ src/components/blocks/index.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withState } from "recompose";
+import styled from "styled-components";
+
+@withState("context", "setContext", null)
+export default class Blocks extends Component {
+  static propTypes = {
+    // Draw function
+    draw: PropTypes.func.isRequired,
+    gridSize: PropTypes.number.isRequired,
+    getContext: PropTypes.func.isRequired,
+
+    // Canvas element state
+    context: PropTypes.object,
+    setContext: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    getContext: canvas => canvas.getContext("2d"),
+    gridSize: 15
+  };
+
+  draw = () => {
+    const { context, draw, gridSize } = this.props;
+
+    for (let x = 0; x < gridSize; ++x) {
+      for (let y = 0; y < gridSize; ++y) {
+        const xDelta = context.canvas.width / gridSize;
+        const yDelta = context.canvas.height / gridSize;
+
+        const drawResult = draw(1 / gridSize * x, 1 / gridSize * y);
+        const updates = Object.assign({ fillStyle: "white" }, drawResult);
+
+        Object.assign(context, updates);
+
+        context.fillRect(xDelta * x, yDelta * y, xDelta, yDelta);
+      }
+    }
+
+    this.animationRequestId = null;
+    this.requestAnimationFrame();
+  };
+
+  requestAnimationFrame = () => {
+    this.cancelAnimationFrame();
+    this.animationRequestId = requestAnimationFrame(this.draw);
+  };
+
+  cancelAnimationFrame = () => {
+    if (!this.animationRequestId) return;
+    cancelAnimationFrame(this.animationRequestId);
+    this.animationRequestId = null;
+  };
+
+  setContext = canvas => {
+    const { context, setContext, getContext } = this.props;
+    const newContext = getContext(canvas);
+
+    if (context === newContext) return;
+
+    newContext.canvas.height = window.innerHeight;
+    newContext.canvas.width = window.innerWidth;
+
+    setContext(newContext);
+
+    this.requestAnimationFrame(this.draw);
+  };
+
+  componentWillUnmount() {
+    this.cancelAnimationFrame();
+  }
+
+  render() {
+    const { className } = this.props;
+    return <canvas className={className} ref={this.setContext} />;
+  }
+}
